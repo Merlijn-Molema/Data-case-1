5b4729e7da317856bc5443b07dbc3b04e45f9450
Merlijn-Molema <165899481+Merlijn-Molema@users.noreply.github.com>
2025-11-07 01:47:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ with tab3:
     ]
 
     # Create Folium map with CartoDB Voyager background
-    m = folium.Map(location=[45, -40], zoom_start=3, tiles="Stadia.StamenWatercolor")
+    m = folium.Map(location=[45, -40], zoom_start=3, tiles="Esri.NatGeoWorldMap")
 
     # Add markers
     for i, (name, coord) in enumerate(coords):
