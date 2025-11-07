ce0c56128b5e6b5747e8a12a0dd0734cb79cf1af
Merlijn-Molema <165899481+Merlijn-Molema@users.noreply.github.com>
2025-11-07 03:45:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import folium
 from streamlit_folium import st_folium
+import pandas as pd
 
 # --- Page setup ---
 st.set_page_config(page_title="Titanic Route Map", page_icon="🛳️", layout="centered")
